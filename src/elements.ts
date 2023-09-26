309ea410dc90777fb126ab6089f39c610e995427
Marek Miałkowski <marekmial15@gmail.com>
2023-09-26 16:25:13 +0200

    added build config

---
+++ src/elements.ts
@@ -0,0 +1,6 @@
+export { Element } from './element/element';
+export { useDocument } from './context/use-document';
+export { useElement } from './context/use-element';
+export { useParent } from './context/use-parent';
+export { useSelf } from './context/use-self';
+export { useSupplier } from './context/use-supplier';
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+import { copyFile } from 'fs/promises';
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    outDir: 'dist',
+    sourcemap: true,
+    lib: {
+      entry: './src/elements.ts',
+      name: 'elements',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    dts({
+      afterBuild() {
+        return copyFile('./package.json', './dist/package.json');
+      },
+    }),
+  ],
+});
