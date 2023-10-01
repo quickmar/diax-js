127e918f1419a201ce5c56a0753df1ed86bd91bb
Marek Miałkowski <marekmial15@gmail.com>
2023-10-01 19:51:01 +0200

    added spec for element decorator

---
+++ test/element.spec.ts
@@ -0,0 +1,79 @@
+import { useElement, useSelf } from '../main';
+import { TestElement, TestFormElement } from './utils';
+
+describe.each([[{ Ctor: TestElement, name: 'test-element' }], [{ Ctor: TestFormElement, name: 'test-form-element' }]])(
+  '@Element',
+  ({ Ctor, name }) => {
+    let element: HTMLElement;
+
+    beforeAll(() => {
+      expect(globalThis.ElementInternals).toBeFalsy();
+      expect(globalThis.HTMLElement?.prototype?.attachInternals).toBeFalsy();
+
+      Object.assign(globalThis, { ElementInternals: class {} });
+      Object.assign(HTMLElement.prototype, {
+        attachInternals: function () {
+          return new ElementInternals();
+        },
+      });
+    });
+
+    afterAll(() => {
+      Object.assign(globalThis, { ElementInternals: undefined });
+      Object.assign(HTMLElement.prototype, { attachInternals: undefined });
+    });
+
+    beforeEach(() => {
+      element = document.createElement(name);
+    });
+
+    it('should create', () => {
+      expect(element).toBeTruthy();
+    });
+
+    it('should constructor have target', () => {
+      const elementCtor = customElements.get(name) ?? {};
+
+      expect(Reflect.get(elementCtor, 'target')).toBe(Ctor);
+    });
+
+    it('should constructor have observedAttributes', () => {
+      const elementCtor = customElements.get(name) ?? {};
+
+      expect(Reflect.get(elementCtor, 'observedAttributes')).toBe(Ctor.observedAttributes);
+    });
+
+    it('should have defined context', () => {
+      useElement(element, () => {
+        expect(useSelf(Ctor)).toBeInstanceOf(Ctor);
+      });
+    });
+
+    it('should have defined HTMLElement in context', () => {
+      useElement(element, () => {
+        expect(useSelf(HTMLElement)).toBe(element);
+      });
+    });
+
+    it('should call callbacks', () => {
+      // connectedCallback
+      document.body.appendChild(element);
+      assertSypCallTimes(1);
+
+      // attributeChangeCallback
+      element.setAttribute('test', 'test');
+      assertSypCallTimes(1);
+
+      // disconnectedCallback
+      document.body.removeChild(element);
+      assertSypCallTimes(1);
+    });
+
+    function assertSypCallTimes(times: number): void {
+      useElement(element, () => {
+        expect(useSelf(Ctor).spy).toBeCalledTimes(times);
+        vi.clearAllMocks();
+      });
+    }
+  },
+);
--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,59 @@
+import { Element, FormElement } from '../main';
 import { CONTEXT } from '../src/context/context';
 import { ElementContext } from '../src/context/element-context';
-import { ContextElement } from '../src/model/elements';
+import { ContextElement, FormElementCallbacks, HTMLElementCallbacks } from '../src/model/elements';
+
+@Element('test-element')
+export class TestElement implements HTMLElementCallbacks {
+  static observedAttributes = ['test'];
+
+  spy = vi.fn();
+
+  connectedCallback(): void {
+    this.spy();
+  }
+  disconnectedCallback(): void {
+    this.spy();
+  }
+  attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown): void {
+    this.spy(name, oldValue, newValue);
+  }
+  adoptedCallback(): void {
+    this.spy();
+  }
+}
+
+@FormElement('test-form-element')
+export class TestFormElement implements HTMLElementCallbacks, FormElementCallbacks {
+  static observedAttributes = ['test'];
+
+  spy = vi.fn();
+
+  connectedCallback(): void {
+    this.spy();
+  }
+  disconnectedCallback(): void {
+    this.spy();
+  }
+  attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown): void {
+    this.spy(name, oldValue, newValue);
+  }
+  adoptedCallback(): void {
+    this.spy();
+  }
+  formAssociatedCallback(form: HTMLFormElement): void {
+    this.spy(form);
+  }
+  formDisabledCallback(disabled: boolean): void {
+    this.spy(disabled);
+  }
+  formResetCallback(): void {
+    this.spy();
+  }
+  formStateRestoreCallback(state: unknown, reason: 'autocomplete' | 'restore'): void {
+    this.spy(state, reason);
+  }
+}
 
 export function createContextElement(tagName: keyof HTMLElementTagNameMap): ContextElement {
   const element = document.createElement(tagName);
