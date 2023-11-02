fd8c8de25464ae212e2fc75b75e384c5268af3b6
Marek Miałkowski <marekmial15@gmail.com>
2023-11-02 17:37:02 +0100

    general changes

--- packages/rendering-element/src/rendering/strategy/document-walker.ts
+++ packages/rendering-element/src/rendering/strategy/document-walker.ts
@@ -1,7 +1,10 @@
+import { DetectionWalker } from './detection-walker';
 import { SubTreeWalker } from './sub-tree-walker';
 
-export class DocumentWalker extends SubTreeWalker {
-  override walk(): void {
-    super.walk(document);
+export class DocumentWalker implements DetectionWalker {
+  constructor(private readonly subTreeWalker: SubTreeWalker) {}
+
+  walk(): void {
+    this.subTreeWalker.walk(document);
   }
 }
