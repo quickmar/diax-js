20804dc2a9de06b8f5450f48334c301427f4696d
Marek Miałkowski <marekmial15@gmail.com>
2025-02-08 21:27:02 +0100

    Added api doc

--- packages/common/src/custom-element/model.ts
+++ packages/common/src/custom-element/model.ts
@@ -1,18 +1,43 @@
 import { ContextHTMLElement } from '../context/model';
 import { NoArgType } from '../model/common';
 
+
 /**
- * @description
- * This interface is used to define the disabled features for all elements that are decorated with the `@Element`, `@RenderingElement`, `@FormElement` decorators.
+ * Interface representing features that can be disabled on an element.
+ *
+ * @interface
+ * @property {Array<'shadow' | 'internals'>} disabledFeatures - An array of features that should be disabled.
+ *                                                              Possible values are:
+ *                                                              - 'shadow': Disables Shadow DOM
+ *                                                              - 'internals': Disables ElementInternals
  */
 export interface DisabledFeatures {
   readonly disabledFeatures: ('shadow' | 'internals')[];
 }
+/**
+ * Represents a constructor interface for a custom element target with callback methods.
+ * 
+ * @template T - Type extending TargetCallbacks defining the callback methods
+ * 
+ * @property {string[]} [observedAttributes] - Array of attribute names to be observed for changes
+ * @property {string[]} [disabledFeatures] - Array of feature names that should be disabled for this element
+ */
 export interface TargetConstructor<T extends TargetCallbacks> extends NoArgType<T> {
   readonly observedAttributes?: string[];
   readonly disabledFeatures?: string[];
 }
 
+/**
+ * Represents a constructor interface for HTML custom elements with specific target callbacks.
+ * 
+ * @typeParam T - The type of target callbacks that this element will support
+ * 
+ * @property target - The constructor for the target that handles element callbacks
+ * @property observedAttributes - Optional array of attribute names to observe for changes
+ * @property disabledFeatures - Optional array of feature names that should be disabled for this element
+ * 
+ * @extends NoArgType<ContextHTMLElement & HTMLElementCallbacks>
+ */
 export interface HTMLElementConstructor<T extends TargetCallbacks>
   extends NoArgType<ContextHTMLElement & HTMLElementCallbacks> {
   readonly target: TargetConstructor<T>;
@@ -20,6 +45,14 @@ export interface HTMLElementConstructor<T extends TargetCallbacks>
   readonly disabledFeatures?: string[];
 }
 
+/**
+ * Represents a set of optional lifecycle callbacks for a target element.
+ * @interface
+ * 
+ * @property {() => void} [init] - Called when the element is initialized.
+ * @property {() => void} [destroy] - Called when the element is destroyed or disconnected.
+ * @property {() => void} [adopt] - Called when the element is adopted into a new document.
+ */
 export type TargetCallbacks = Partial<{
   init(): void;
   destroy(): void;
@@ -27,6 +60,17 @@ export type TargetCallbacks = Partial<{
 }> &
   object;
 
+/**
+ * Interface representing lifecycle callbacks for custom HTML elements.
+ * These callbacks are automatically called by the browser at specific moments in an element's lifecycle.
+ * 
+ * @interface HTMLElementCallbacks
+ * 
+ * @property {() => void} connectedCallback - Called when the element is inserted into the DOM
+ * @property {() => void} disconnectedCallback - Called when the element is removed from the DOM
+ * @property {(name: string, oldValue: unknown, newValue: unknown) => void} attributeChangedCallback - Called when an observed attribute is changed, added, or removed
+ * @property {() => void} adoptedCallback - Called when the element is moved to a new document
+ */
 export interface HTMLElementCallbacks {
   connectedCallback(): void;
   disconnectedCallback(): void;
@@ -34,6 +78,14 @@ export interface HTMLElementCallbacks {
   adoptedCallback(): void;
 }
 
+/**
+ * A decorator type for custom elements that can be applied to classes.
+ * 
+ * @typeParam T - A constructor type that extends TargetConstructor with callback methods
+ * @param target - The class constructor to decorate
+ * @param context - The decorator context provided by TypeScript
+ * @returns void
+ */
 export type CustomElementDecorator = <T extends TargetConstructor<TargetCallbacks>>(
   target: T,
   context: ClassDecoratorContext,
--- packages/common/src/destroying/support.ts
+++ packages/common/src/destroying/support.ts
@@ -1,5 +1,9 @@
 import { Cleanable } from './model';
 
+/**
+ * Represents an action that can be scheduled for execution during idle time.
+ * The action can only be scheduled once and subsequent attempts will be ignored.
+ */
 export class DestroyAction {
   private closed = false;
 
@@ -12,6 +16,20 @@ export class DestroyAction {
     }
   }
 }
+
+/**
+ * Type guard that checks if an object implements the {@link Cleanable} interface.
+ * 
+ * @param object - The object to check
+ * @returns True if the object has a destroy() method, false otherwise
+ * 
+ * @example
+ * ```typescript
+ * if (isCleanable(someObject)) {
+ *   someObject.destroy();
+ * }
+ * ```
+ */
 export function isCleanable(object: unknown): object is Cleanable {
   return !!object && typeof object === 'object' && 'destroy' in object && typeof object['destroy'] === 'function';
 }
